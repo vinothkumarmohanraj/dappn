0db950173b11d9e9c1d6f3da951fcbd33c9f36f3
vinothkumarmohanraj <85845728+vinothkumarmohanraj@users.noreply.github.com>
2024-09-13 17:32:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask,render_template,request
+from flask import Flask, request, render_template, redirect, url_for, flash, session
 
-app=Flask(__name__)
+app = Flask(__name__)
+app.secret_key = '18723h3'  
 
-@app.route('/',methods=["get","post"])
+@app.route('/')
 def index():
-    return render_template('index.html')
+    if 'username' in session:
+    
+        return render_template('dapp.html', username=session['username'])
+    else:
+      
+        return redirect(url_for('login'))
 
-if __name__=='__main__':
-    app.run()
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+     if request.method == 'POST':
+       
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        session['username'] = username
+        return redirect(url_for('index'))  
+
+     return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)  
+    return redirect(url_for('login'))  
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", 8080)
